c0b05f54c84616259ad603a9b8f6d36796d5ab89
Drew Conly <atconly@gmail.com>
2015-05-25 14:54:41 -0600

    with challenges

--- fibber.rb
+++ fibber.rb
@@ -1,6 +1,6 @@
 
   def fib(n)
-    return 0 if n == 0
+    return [0] if n == 0
 
     x = 0
     y = 1
@@ -15,13 +15,23 @@
     fibber
   end
 
-  def jagged_output(fibber)
-  
+
+answer = fib(28)
+
+
+  answer.each_slice(5) do |n|
+    puts n.join(" ")
   end
 
+  answer.each_slice(5) do |a, b, c, d, e|
+    puts [a,b,c,d,e].map {|a| a.to_s.rjust(4)}.join(" ")
+  end
 
+  special_length = answer[-1].to_s.length
 
-print jagged_output(fib(14))
+  answer.each_slice(5) do |a, b, c, d, e|
+    puts [a,b,c,d,e].map {|a| a.to_s.rjust(special_length + 1)}.join(" ")
+  end
 
 
 
